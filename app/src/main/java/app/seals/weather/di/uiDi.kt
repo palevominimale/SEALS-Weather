b7378b15494c406c0bfc719133fa6896bbe56679
Valerii Makarov <badtri.orchestra@gmail.com>
2022-09-10 22:11:43 +0200

    optimized sharedViewModel DI added apk

--- app/src/main/java/app/seals/weather/di/uiDi.kt
+++ app/src/main/java/app/seals/weather/di/uiDi.kt
@@ -1,13 +1,9 @@
 package app.seals.weather.di
 
-import app.seals.weather.domain.usecases.forecast.LoadDailyForecast
-import app.seals.weather.domain.usecases.forecast.LoadHourlyForecast
-import app.seals.weather.domain.usecases.forecast.RefreshForecast
 import app.seals.weather.ui.vm.SharedViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-import org.koin.java.KoinJavaComponent
 
 val uiDi = module {
     viewModel {
@@ -23,10 +19,3 @@ val uiDi = module {
         )
     }
 }
-
-//private val loadDailyForecast : LoadDailyForecast
-//        by KoinJavaComponent.inject(LoadDailyForecast::class.java)
-//private val loadHourlyForecast : LoadHourlyForecast
-//        by KoinJavaComponent.inject(LoadHourlyForecast::class.java)
-//private val retrofit : RefreshForecast
-//        by KoinJavaComponent.inject(RefreshForecast::class.java)
