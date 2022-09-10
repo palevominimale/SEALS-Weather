d89156f31a506c80b3f93c5e7f1435b768ade534
Valerii Makarov <badtri.orchestra@gmail.com>
2022-09-10 21:50:18 +0200

    removed unused imports

--- app/src/main/java/app/seals/weather/domain/usecases/location/UpdateLocation.kt
+++ app/src/main/java/app/seals/weather/domain/usecases/location/UpdateLocation.kt
@@ -1,14 +1,7 @@
 package app.seals.weather.domain.usecases.location
 
-import android.location.LocationManager
-import android.util.Log
 import app.seals.weather.data.repos.SettingsRepository
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
 
 class UpdateLocation(
     private val fusedLocationProviderClient: FusedLocationProviderClient,
